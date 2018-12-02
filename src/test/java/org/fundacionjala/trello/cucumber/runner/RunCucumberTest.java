05f073c05717fe354fd22a155e0b5f82b8e79725
carledriss <carledriss@gmail.com>
2018-12-02 10:49:12 -0400

    Initial project

---
+++ src/test/java/org/fundacionjala/trello/cucumber/runner/RunCucumberTest.java
@@ -0,0 +1,40 @@
+package org.fundacionjala.trello.cucumber.runner;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+import org.fundacionjala.core.ui.DriverManager;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+
+/**
+ * Class which runs all features.
+ */
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"org.fundacionjala.trello.cucumber"},
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports/cucumber-pretty",
+                "json:target/cucumber-reports/CucumberTestReport.json",
+                "rerun:target/cucumber-reports/rerun.txt"
+        })
+public class RunCucumberTest extends AbstractTestNGCucumberTests {
+
+
+    /**
+     * this method execute before the tests.
+     */
+    @BeforeClass
+    public void open() {
+
+    }
+
+    /**
+     * this method close the browser after the features finish.
+     */
+    @AfterClass
+    public void close() {
+        DriverManager.getInstance().getDriver().close();
+    }
+}
