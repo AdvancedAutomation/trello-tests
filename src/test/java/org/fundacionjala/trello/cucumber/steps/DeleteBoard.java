e4dc91acc39d4d58916540590194eb3ad852ea75
RogerAlvarezM <roger.alvarez@fundacion-jala.org>
2018-12-19 23:18:55 -0400

    update1

--- src/test/java/org/fundacionjala/trello/cucumber/steps/DeleteBoard.java
+++ src/test/java/org/fundacionjala/trello/cucumber/steps/DeleteBoard.java
@@ -1,4 +1,11 @@
 package org.fundacionjala.trello.cucumber.steps;
 
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+
 public class DeleteBoard {
+    @Given("I select board {string}")
+    public void iSelectBoard(String arg0) {
+
+    }
 }
