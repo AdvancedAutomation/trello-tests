a36d1944b09999d6d6f65046140918cd2a68282d
wilson lopez <wiltrouble@at08-linux-gui-automation.novalocal>
2019-01-14 14:46:23 -0400

    sonar fix

--- src/test/java/org/fundacionjala/trello/cucumber/steps/ui/ListSteps.java
+++ src/test/java/org/fundacionjala/trello/cucumber/steps/ui/ListSteps.java
@@ -104,5 +104,6 @@ public class ListSteps {
          */
         @Then("I should see the list in the board:")
         public void iShouldSeeTheListInTheBoard(final Map<String, String> data) {
+            //To do
         }
     }
